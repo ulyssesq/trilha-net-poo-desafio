09eab3f3f0f4f90273ff0bcbe0451272025774fb
Ulysses Queiroz <ulyssesq@gmail.com>
2024-01-09 11:10:08 -0300

    feat: implementando o desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Instanciando um Nokia");
+Smartphone nokia = new Nokia("23456789", "Galaxy S23", "223452234522345", 256);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Facebook");
+
+Console.WriteLine("Instanciando um Iphone");
+Smartphone iphone = new Iphone("12345678", "15 Pro", "123451234512345", 256);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp");
